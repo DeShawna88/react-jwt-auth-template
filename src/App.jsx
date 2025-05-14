a6053419d858dd17239f72e2efd75dd08993194c
Shawnade <deshawnagalloway@yahoo.com>
2025-05-14 14:03:23 -0400

    commit template

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 // src/App.jsx
-
+// Import useContext
+import { useContext } from 'react';
+import { Routes, Route } from 'react-router'; // Import React Router
 import NavBar from './components/NavBar/NavBar';
+// Import the SignUpForm component
+import SignUpForm from './components/SignUpForm/SignUpForm';
+import SignInForm from './components/SignInForm/SignInForm';
+// Import the Landing and Dashboard components
+import Landing from './components/Landing/Landing';
+import Dashboard from './components/Dashboard/Dashboard';
+// Import the UserContext
+import { UserContext } from './contexts/UserContext';
+
 
 const App = () => {
+  const { user } = useContext(UserContext);
+
   
   return (
     <>
       <NavBar />
-      <h1>Hello, friend!</h1>
+      {/* Add the Routes component to wrap our individual routes*/}
+      <Routes>
+        {/* Add the new `/` route! */}
+        <Route path='/' element={user ? <Dashboard /> : <Landing /> } />
+        <Route path='/sign-up' element={<SignUpForm />} />
+        <Route path='/sign-in' element={<SignInForm />} />
+      </Routes>
     </>
   );
 };
